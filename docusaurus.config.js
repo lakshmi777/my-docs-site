5030a1f4d5fc33584f4019363a68d1119791885f
Lakshmi H <35521883+lakshmi777@users.noreply.github.com>
2026-02-19 15:05:13 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,11 +20,8 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
-
+ url: 'https://lakshmi777.github.io',
+baseUrl: '/my-docs-site/',
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'facebook', // Usually your GitHub org/user name.
